8d7b1488c55ea59b93d919899ce8c87655a5fa3f
Lucas A. <andreattovysk@tutanota.de>
2023-09-30 22:44:28 -0300

    add layout page

--- lib/pensieve_web/controllers/page_controller.ex
+++ lib/pensieve_web/controllers/page_controller.ex
@@ -6,4 +6,9 @@ defmodule PensieveWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def about(conn, _params) do
+    render(conn, :about)
+  end
+
 end
