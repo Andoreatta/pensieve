c70f7adc6a47dfd213cb9c0130384e26fd362b2f
Lucas A. <andreattovysk@tutanota.de>
2023-10-08 18:51:32 -0300

    extract <.information> component

--- lib/pensieve_web/components/memory_information.ex
+++ lib/pensieve_web/components/memory_information.ex
@@ -1,16 +1,12 @@
-defmodule PensieveWeb.MemoryHTML do
-  use PensieveWeb, :html
+defmodule PensieveWeb.MemoryInformation do
+  use Phoenix.Component
 
-  embed_templates "/memory_html/*"
   attr :memories, :list, required: true
-
   def information(assigns) do
     ~H"""
     <div class="italic mb-4">
-      These are the memories in your Pensieve
       There are <%= length(@memories) %> memories in your Pensieve
     </div>
     """
   end
-
 end
